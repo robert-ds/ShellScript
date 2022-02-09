00984fc8ab052ec007c459c68371ec7ba07a71b0
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-02-09 10:11:10 -0400

    restructurando el codigo

--- scriptPostInstalling.sh
+++ scriptPostInstalling.sh
@@ -1,21 +1,12 @@
-# Hacer un script que permita seleccionar atravez de un menu en pantalla (1) Recursos esenciales del sistema, (2) un pack de tools y apps basicas para el sistema,
-# (3) huevos de pascua, (4) salir. Además que muestre por pantalla el progreso de la acción elegida y que al finalizar vuelva a ejecutar el menu de seleccion.
-
+#Script para instalar el software necesario luego de instalar algun OS basado en debian.
 #!/bin/bash
 
 # Robert Vásquez 2021
 # Versión 1.0
 
-#Menú de Opciones
-main_menu() {
-  echo '1. Instalar paquetes esenciales del sistema, includo ubuntu-restricted-extras.';
-  echo '2. Instalar pack de paquetes tools y apps básicas como: elinks, git, etc. ';
-  echo '3. Instalar huevos de pascuas como: sl, cowsay, figlet, etc.  ';
-  echo '4. Salir ';
-  read -p '' opc;
-  echo '';
-
-  case $opc in
+#Opciones
+cases(){
+    case $opc in
     1)
       echo 'Selecionaste la opción 1. Se inatalarán los paquetes esenciales del sistema'; sleep 4s; clear;
       sudo apt-get install software-properties-common build-essential ubuntu-restricted-extras gnome-tweak-tool net-tools -y;
@@ -33,15 +24,28 @@ main_menu() {
       main_menu;;
     4)
       echo 'Selecionaste salir.'; sleep 1s; clear;
-      echo 'CHAO!';sleep 3s; exit;;
+      echo 'Hasta Luego!';sleep 3s; exit;;
     *)
       echo 'Porfavor Seleciona una opción valida'; sleep 2s; clear;
       main_menu;
   esac
 }
 
+#Menú de Opciones
+main_menu(){
+  echo '1. Instalar paquetes esenciales del sistema, includo ubuntu-restricted-extras.';
+  echo '2. Instalar pack de paquetes tools y apps básicas como: elinks, git, etc. ';
+  echo '3. Instalar huevos de pascuas como: sl, cowsay, figlet, etc.  ';
+  echo '4. Salir ';
+  read -p '' opc;
+  echo '';
+
+  cases;
+
+}
+
 installing_state(){
-  if [$?]; then
+  if [echo $? -eq 0]; then
     echo 'Todo se Instalo Satisfactoriamente';
     sleep 3s; clear;
   else
@@ -56,10 +60,10 @@ installing_state(){
   clear;
   echo '.............:::::Autor: Robert Vásquez. 2021::::::::::::............'; sleep 3s;
   echo '.............:::::Primero se actualizara el Sistema:::::............'; sleep 3s;
-  #sudo apt-get update && sudo apt-get upgrade -y;
+  sudo apt-get update && sudo apt-get upgrade -y;
 
   #Comprobar si la actualización anterior fue ejecutada con exito
-  if [$?]; then
+  if [echo $? -eq 0]; then
     clear;
     echo 'EL SISTEMA HA SIDO ACTUALIZADO';
     sleep 4s; clear;
@@ -68,7 +72,7 @@ installing_state(){
     echo 'Ha ocurrido un problema';
     echo 'Comprueba tu conexion a internet';
     sleep 4s; clear;
-    #echo 'CHAO!';sleep 4s; exit;
+    echo 'Hasta Luego';sleep 4s; exit;
   fi
 
   #Mensaje antesala para el menú de opciones
@@ -82,4 +86,4 @@ installing_state(){
 
   main_menu;
 
-#instalar opcional GNOME Shell integration
+#instalar opcional, GNOME Shell integration
